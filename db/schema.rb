6351121c7c54aff79a6274b809816f9ca768c28b
Marcos Andre <m2c.andre@gmail.com>
2016-07-11 13:45:19 +0000

    Usuário marca vagas em destaque

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160708202808) do
+ActiveRecord::Schema.define(version: 20160711130834) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160708202808) do
     t.string   "location"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.boolean  "featured"
   end
 
 end
